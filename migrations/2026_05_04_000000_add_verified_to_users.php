4c919968cfe603adef7526024a164d936c213025
Ramon <ramon@ramonguilherme.com.br>
2026-05-21 22:17:51 -0300

    fix: corrigir tipo de coluna 'verified_by' para 'integer unsigned' na tabela de usuários

--- migrations/2026_05_04_000000_add_verified_to_users.php
+++ migrations/2026_05_04_000000_add_verified_to_users.php
@@ -5,5 +5,5 @@ use Flarum\Database\Migration;
 return Migration::addColumns('users', [
     'is_verified' => ['boolean', 'default' => false],
     'verified_at' => ['dateTime', 'nullable' => true],
-    'verified_by' => ['unsignedInteger', 'nullable' => true],
+    'verified_by' => ['integer', 'unsigned' => true, 'nullable' => true],
 ]);
